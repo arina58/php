a4517d3a0f6926e3f83b883198a925e23d0d4889
Arina Iljinih <iljinih.ar@yandex.ru>
2022-03-21 11:10:47 +0700

    через if

--- form.php
+++ form.php
@@ -1,7 +1,20 @@
 <?php
     header('Content-type: text/html; charset=utf-8');
-    $name = $_GET['n'];
-    $surname = $_GET['s'];
-    $result = '<h1>Вас зовут: '.$name .' '. $surname.'</h1>';
+    $x = $_GET['x'];
+    $y = $_GET['y'];
+    $d = $_GET['d'];
+    $result = $x.$d.$y.'=';
+    if ($d == '+'){
+        $result .= $x + $y;
+    }
+    if ($d == '-'){
+        $result .= $x - $y;
+    }
+    if ($d == '*'){
+        $result .= $x * $y;
+    }
+    if ($d == '/'){
+        $result .= $x / $y;
+    }
     echo $result;
 ?>
